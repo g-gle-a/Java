b5ef0f6a3569c633ff5b869441026e6a2730a90c
g-gle-a <57418181+g-gle-a@users.noreply.github.com>
2022-05-14 09:44:54 +0100

    Add files via upload

--- MatrixMult.java
+++ MatrixMult.java
@@ -14,67 +14,46 @@ public class MatrixMult {
 	      File myObj = new File(filename.nextLine ()); // or var filename
 	      System.out.println("Input file 1 succesfully open \n ---- \n");
 	      
-	      System.out.println("Input file name 2");
-	      Scanner filename2 = new Scanner(System.in);
-	      File myObj2 = new File(filename2.nextLine ()); // or var filename
-	      System.out.println("Input file 2 succesfully open \n ---- \n");
-	      
-	      System.out.println("Output file name");
-	      Scanner fileOut = new Scanner(System.in);
-	      FileWriter myWriter = new FileWriter(fileOut.nextLine()); 
-	      System.out.println("Output file  succesfully open \n ---- \n");
+	      //Start reading into memory
 	      Scanner myReader = new Scanner(myObj);
 	      myReader.useDelimiter(",");
-//Start reading into memory
 	      ArrayList<String> list = new ArrayList<String>();
 	      while (myReader.hasNextLine()) {
 	    	  list.add(myReader.nextLine());
 	      }  
 	      
-	      // Warning I don't know the correct order of these two sentences :) 
-	      myReader.close();
-	      filename.close();
-
-	      // Knowing  the dimensions of the matrix
+  	      // Knowing  the dimensions of the matrix
 	      int nRows = list.size();
           String firstline = list.get(0); 	
 	      String[] cols= firstline.split(",");
-	      
-	      int nCols = 0;
-	      nCols = cols.length;
-      
-	      
+	      int nCols = cols.length;
 	      float[][] myMatrix1 = new float[nRows][nCols];
 	      int i=0;
 	      for (String csvLine : list) {
 	    	  String[] csvRead = csvLine.split(",");
 	    	  for (int j=0; j<nCols; j++) {
 	    		 myMatrix1[i][j] = Float.parseFloat(csvRead[j]);  
-	    		
 	    	  }
 	    	  i++;
 	      }
 	      
-	      
-	      
+	      System.out.println("Input file name 2");
+	      Scanner filename2 = new Scanner(System.in);
+	      File myObj2 = new File(filename2.nextLine ()); // or var filename
+	      System.out.println("Input file 2 succesfully open \n ---- \n");
 	      Scanner myReader2 = new Scanner(myObj2);
 	      myReader2.useDelimiter(",");
-//Start reading into memory
+	      
+	      //Start reading into memory
 	      ArrayList<String> list2 = new ArrayList<String>();
 	      while (myReader2.hasNextLine()) {
 	    	  list2.add(myReader2.nextLine());
 	      }  
 	      
-	      // Warning I don't know the correct order of these two sentences :) 
-	      myReader2.close();
-	      filename2.close();
-	      
 	      int nRows2 = list2.size();
           firstline = list2.get(0); 	
 	      String[] cols2= firstline.split(",");
-	      
-	      int nCols2 = 0;
-	      nCols2 = cols2.length;
+	      int nCols2 = cols2.length;
       
 	      float[][] myMatrix2 = new float[nRows2][nCols2];
 	      i=0;
@@ -82,11 +61,20 @@ public class MatrixMult {
 	    	  String[] csvRead = csvLine.split(",");
 	    	  for (int j=0; j<nCols; j++) {
 	    		 myMatrix2[i][j] = Float.parseFloat(csvRead[j]);  
-	    		
 	    	  }
 	    	  i++;
 	      }
 	      
+	      System.out.println("Output file name");
+	      Scanner fileOut = new Scanner(System.in);
+	      FileWriter myWriter = new FileWriter(fileOut.nextLine()); 
+	      System.out.println("Output file  succesfully open \n ---- \n");
+
+	      // Warning I don't know the correct order of these two sentences :) 
+	      myReader.close();
+	      myReader2.close();
+	      filename.close();
+	      filename2.close();
 	      // Matrix Multiplication here!!!
 	      if (nCols != nRows2) {
 	    	  System.out.println("Can't multiply these matrices, incompatible rows and cols sizes:" + nCols + "--" + nRows2);
@@ -99,14 +87,10 @@ public class MatrixMult {
 	    			  data += myMatrix1[i][k] * myMatrix2[k][j];
 	    		  }
 	    		  myNumbers[i][j] = data;
-		    	  System.out.println(i + "**" + j + "***" + myNumbers[i][j] );
-		    	  System.out.println(" *** " );  
 	    	  }
 	      }
-	      
-	      
+	      System.out.println("Matrices Multiplied \n ---- \n");
 	      // Final Matrix output
-
 	   for (i=0; i < nRows ; i++) {
 	    	  for ( int j=0; j<nCols2; j++) {
 	    		 String data = Float.toString (myNumbers[i][j]) + ',';
@@ -114,6 +98,7 @@ public class MatrixMult {
 	    	  }
 	    	  myWriter.write('\n');
 	     }
+	      System.out.println("Output succesfull \n ---- \n.... All Done ......\n ---- \n");
 	      myWriter.close();
 	      fileOut.close();
 	    } catch (IOException e) {
